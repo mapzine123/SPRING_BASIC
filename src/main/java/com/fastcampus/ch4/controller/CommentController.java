5812caf337a4577f1087f72593c3c6dfc62c8260
DESKTOP-KAT0B72\mapzi <mapzine123@naver.com>
2022-04-03 22:43:37 +0900

    CommentController Delete

--- src/main/java/com/fastcampus/ch4/controller/CommentController.java
+++ src/main/java/com/fastcampus/ch4/controller/CommentController.java
@@ -5,14 +5,35 @@ import com.fastcampus.ch4.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 public class CommentController {
     @Autowired
     private CommentService service;
+    
+    // 지정된 댓글 삭제
+    @DeleteMapping("/comments/{cno}") // comments/1 <-- 삭제할 댓글 번호
+    @ResponseBody
+    public ResponseEntity<String> remove(@PathVariable Integer bno, Integer cno, HttpSession session) {
+        String commenter = (String)session.getAttribute("id");
+        try {
+            int rowCnt = service.remove(cno, bno, commenter);
 
+            if(rowCnt != 1) {
+              throw new Exception("Delete Failed");
+            }
+            return new ResponseEntity<>("DEL_OK", HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 
 
 
